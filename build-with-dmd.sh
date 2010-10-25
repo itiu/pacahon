e9c33bb243f8ec1143940cae28f9494664c3fc67
Itiu <ValeriyBushenev@gmail.com>
2010-10-25 18:38:18 +0400

    ++

--- build-with-dmd.sh
+++ build-with-dmd.sh
@@ -30,7 +30,7 @@ rm build/src/test_send.d
 
 git log -1 --pretty=format:"module myversion; public static char[] author=cast(char[])\"%an\"; public static char[] date=cast(char[])\"%ad\"; public static char[] hash=cast(char[])\"%h\";">myversion.d
 
-rm Pacahon*
+rm Pacahon-*
 ~/dmd/linux/bin/dmd -version=D1 -debug -g myversion.d build/src/trioplax/memory/*.d build/src/trioplax/mongodb/*.d build/src/trioplax/*.d build/src/*.d lib/libzmq.a lib/libstdc++.a lib/libuuid.a -ofPacahon-D1
 ~/dmd2/linux/bin/dmd -version=D2 -debug -g myversion.d build/src/trioplax/memory/*.d build/src/trioplax/mongodb/*.d build/src/trioplax/*.d build/src/*.d lib/libzmq.a lib/libstdc++.a lib/libuuid.a -ofPacahon-D2
 rm *.o
