b2ae9ace669a9a0278a07e885104e2c08e364d60
Itiu <ValeriyBushenev@gmail.com>
2011-06-20 11:30:01 +0400

    Revert "Revert "struct Predicate -> class Predicate""
    This reverts commit 6c316c7196839a1e0329ecbb583b408be05c9cad.

--- build-with-dmd.sh
+++ build-with-dmd.sh
@@ -1,3 +1,5 @@
+DMD=~/dmd2-053/linux/bin32/dmd
+
 cp -v -r src/* build/src
 
 git log -1 --pretty=format:"module myversion; public static char[] author=cast(char[])\"%an\"; public static char[] date=cast(char[])\"%ad\"; public static char[] hash=cast(char[])\"%h\";">myversion.d
@@ -8,12 +10,13 @@ rm *.io
 
 bf=build/src
 
+
 #~/dmd/linux/bin/dmd -version=D1 -debug -g myversion.d build/src/trioplax/memory/*.d build/src/trioplax/mongodb/*.d build/src/trioplax/*.d build/src/*.d lib/libzmq.a lib/libstdc++.a lib/libuuid.a -ofPacahon-D1
 #-version=trace_turtle_parser
-~/dmd2-053/linux/bin32/dmd -O -version=D2 -version=dmd2_053 -version=trace_turtle_parser0 myversion.d \
+$DMD -O -version=D2 -version=dmd2_053 -version=trace_turtle_parser0 -inline -d \
+lib/dmdscriptlib.a lib/libzmq.a lib/libczmq.a lib/libstdc++.a lib/libuuid.a \
+myversion.d \
 $bf/trioplax/mongodb/*.d $bf/trioplax/memory/*.d $bf/rt/util/*.d $bf/trioplax/*.d $bf/*.d \
 $bf/tango/util/uuid/*.d  $bf/tango/core/*.d $bf/tango/text/convert/*.d $bf/tango/util/digest/*.d $bf/tango/math/random/*.d \
-lib/libzmq.a lib/libczmq.a lib/libstdc++.a lib/libuuid.a -ofPacahon-D2-1
+-ofPacahon-D2-1
 rm *.o
-
- 
