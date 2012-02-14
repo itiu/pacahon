38a4f9ef783e802528bcc859d7a342a9fc8b26e2
Itiu <ValeriyBushenev@gmail.com>
2012-02-14 16:27:39 +0400

    * lib64

--- build-dmd32.sh
+++ build-dmd32.sh
@@ -2,7 +2,7 @@ DMD=dmd
 
 VERSION_MAJOR=1
 VERSION_MINOR=26
-VERSION_PATCH=0
+VERSION_PATCH=6
 
 cp -v -r src/* build/src
 
--- src/myversion.d
+++ src/myversion.d
@@ -1,1 +1,1 @@
-module myversion; public static string major="1"; public static string minor="26"; public static string patch="0"; public static string author="Itiu"; public static string date="Wed Feb 1 18:56:11 2012 +0400"; public static string hash="d34a4a7";
+module myversion; public static string major="1"; public static string minor="26"; public static string patch="6"; public static string author="Itiu"; public static string date="Tue Feb 14 16:06:48 2012 +0400"; public static string hash="6758675";
