1005800c067272c34c5f7c59118c3a1814b2b02a
Itiu <ValeriyBushenev@gmail.com>
2013-12-17 18:17:29 +0400

    * dependency: lmdb 0.9.10

--- src/pacahon/myversion.d
+++ src/pacahon/myversion.d
@@ -1,1 +1,1 @@
-module pacahon.myversion; public static string major="1"; public static string minor="37"; public static string patch="0"; public static string author="Itiu"; public static string date="Fri Dec 13 17:43:06 2013 +0400"; public static string hash="b7524fa";
+module pacahon.myversion; public static string major="1"; public static string minor="37"; public static string patch="0"; public static string author="Itiu"; public static string date="Tue Dec 17 17:23:39 2013 +0400"; public static string hash="7d817f6";
