be005e8ca3a2ecec12371fc3e2a8c3801d48960e
Itiu <ValeriyBushenev@gmail.com>
2014-03-28 17:09:52 +0400

    * readme

--- src/pacahon/myversion.d
+++ src/pacahon/myversion.d
@@ -1,3 +1,1 @@
-module pacahon.myversion; public static string major  = "1"; public static string minor = "38"; public static string patch = "4";
-public static string                           author = "Itiu"; public static string date = "Wed Mar 5 11:27:40 2014 +0400";
-public static string                           hash   = "5692fda";
+module pacahon.myversion; public static string major="1"; public static string minor="38"; public static string patch="4"; public static string author="Itiu"; public static string date="Fri Mar 28 16:45:40 2014 +0400"; public static string hash="4072da0";
