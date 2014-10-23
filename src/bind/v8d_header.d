950c82ee2cfb0be9635bd943c6f15f7499206905
itiu <itiu@itiu-OptiPlex-390.(none)>
2014-10-23 11:57:37 +0400

    * v8 binding: return null if get_individual return null

--- src/bind/v8d_header.d
+++ src/bind/v8d_header.d
@@ -76,9 +76,14 @@ extern (C++)_Buff * read_individual(const char *_ticket, int _ticket_length, con
             if (g_context !is null)
             {
                 string icb = g_context.get_individual_from_storage(uri);
-                tmp_individual.data   = cast(char *)icb;
-                tmp_individual.length = cast(int)icb.length;
-                return &tmp_individual;
+                if (icb !is null)
+                {
+                	tmp_individual.data   = cast(char *)icb;
+                	tmp_individual.length = cast(int)icb.length;
+                	return &tmp_individual;
+                }
+                else
+                	return null;
             }
             return null;
         }
